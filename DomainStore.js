aba006f3d5c4d79178a5376524dde8991a12eb23
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2018-08-20 22:33:41 +0300

    More random images

--- DomainStore.js
+++ DomainStore.js
@@ -1,26 +1,29 @@
 import { observable } from 'mobx'
 import Food from './Food'
-import { POSITIVE, NEGATIVE } from './Reaction'
+import { NEGATIVE, POSITIVE } from './Reaction'
 
 class DomainStore {
   @observable foodsOfDay = []
 
   init() {
     this.foodsOfDay = [
-      new Food('https://lorempixel.com/400/200', POSITIVE),
-      new Food('https://lorempixel.com/401/201', POSITIVE),
-      new Food('https://lorempixel.com/402/202', NEGATIVE),
-      new Food('https://lorempixel.com/403/203', POSITIVE),
-      new Food('https://lorempixel.com/404/204', POSITIVE),
-      new Food('https://lorempixel.com/405/205', POSITIVE),
-      new Food('https://lorempixel.com/406/206', POSITIVE),
-      new Food('https://lorempixel.com/407/207', NEGATIVE),
-      new Food('https://lorempixel.com/408/208', POSITIVE),
-      new Food('https://lorempixel.com/409/209', POSITIVE),
-      new Food('https://lorempixel.com/410/210', NEGATIVE),
-      new Food('https://lorempixel.com/411/211', POSITIVE)
+      new Food('ads', randomImage(), POSITIVE),
+      new Food('qwe', randomImage(), POSITIVE),
+      new Food('zxc', randomImage(), NEGATIVE),
+      new Food('ert', randomImage(), POSITIVE),
+      new Food('dfg', randomImage(), POSITIVE),
+      new Food('vbn', randomImage(), POSITIVE),
+      new Food('wer', randomImage(), POSITIVE),
+      new Food('sdf', randomImage(), NEGATIVE),
+      new Food('cvb', randomImage(), POSITIVE),
+      new Food('rty', randomImage(), POSITIVE),
+      new Food('fgh', randomImage(), NEGATIVE),
+      new Food('bnm', randomImage(), POSITIVE)
     ]
   }
 }
+
+const randomImage = () => `https://dummyimage.com/300x100/${randomColor()}/${randomColor()}`
+const randomColor = () => Math.floor(Math.random()*16777215).toString(16)
 
 export default new DomainStore()
