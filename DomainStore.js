0bb0de135412f325b0c1601a6cfb9916fb04feed
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2018-08-20 16:22:54 +0300

    Add more dummy food

--- DomainStore.js
+++ DomainStore.js
@@ -10,7 +10,15 @@ class DomainStore {
       new Food('https://lorempixel.com/400/200', POSITIVE),
       new Food('https://lorempixel.com/401/201', POSITIVE),
       new Food('https://lorempixel.com/402/202', NEGATIVE),
-      new Food('https://lorempixel.com/403/203', POSITIVE)
+      new Food('https://lorempixel.com/403/203', POSITIVE),
+      new Food('https://lorempixel.com/404/204', POSITIVE),
+      new Food('https://lorempixel.com/405/205', POSITIVE),
+      new Food('https://lorempixel.com/406/206', POSITIVE),
+      new Food('https://lorempixel.com/407/207', NEGATIVE),
+      new Food('https://lorempixel.com/408/208', POSITIVE),
+      new Food('https://lorempixel.com/409/209', POSITIVE),
+      new Food('https://lorempixel.com/410/210', NEGATIVE),
+      new Food('https://lorempixel.com/411/211', POSITIVE)
     ]
   }
 }
