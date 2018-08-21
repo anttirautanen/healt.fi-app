395cd927087ecff4f59b52705cdec904369adbde
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2018-08-21 07:19:21 +0300

    Show reaction on top of food image

--- FoodList.js
+++ FoodList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { FlatList, ImageBackground, StyleSheet, Text } from 'react-native'
+import { FlatList, Image, ImageBackground, StyleSheet, View } from 'react-native'
+import { ALMOST_WHITE } from './colors'
 import { inject, observer } from './node_modules/mobx-react/native'
+import { POSITIVE } from './Reaction'
 
 @inject('store')
 @observer
@@ -16,15 +18,32 @@ class FoodList extends Component {
   }
 }
 
-const FoodListItem = ({ item: food }) =>
-  <ImageBackground source={{ uri: food.imageUrl }} style={styles.image}>
-    <Text>lol inside</Text>
-  </ImageBackground>
+const FoodListItem = ({ item: food }) => {
+  const reactionIcon = food.reaction === POSITIVE ? require('./assets/icons/thumbsUp.png') : require('./assets/icons/thumbsDown.png')
+  return (
+    <ImageBackground source={{ uri: food.imageUrl }} style={styles.image}>
+      <View style={styles.reactionContainer}><Image source={reactionIcon}/></View>
+    </ImageBackground>
+  )
+}
 
 const styles = StyleSheet.create({
   image: {
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    flex: 1,
+    padding: 20,
     width: '100%',
-    height: 100
+    height: 90
+  },
+  reactionContainer: {
+    alignItems: 'center',
+    flexGrow: 0,
+    flexShrink: 0,
+    justifyContent: 'center',
+    backgroundColor: ALMOST_WHITE,
+    width: 50
   }
 })
 
